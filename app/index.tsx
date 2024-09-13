27b366d5ca1c3844b9f5121165ed8cc42ef1b688
NataliaJeszke <nataliajeszke8@gmail.com>
2024-09-13 19:22:17 +0200

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import animation from '../assets/animation/animdog.json';
+import LottieView from 'lottie-react-native';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ height: 200, width: 200 }}>
+      <LottieView style={{flex:1}} source={require('../assets/animation/animdog.json')} autoPlay loop />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
